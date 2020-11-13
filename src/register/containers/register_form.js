35c29ac19546a5a24a1a85241c8ee5219e62e6ed
Francklin Marroquin <francklin.marroquin@outlook.com>
2020-11-13 10:44:11 -0600

    including axios

--- src/register/containers/register_form.js
+++ src/register/containers/register_form.js
@@ -1,31 +1,80 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
 class RegisterForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: '',
+      lastname: '',
+      email: '',
+      password: '',
+      phone: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+
+    const user = {
+      nombre: this.state.name,
+      apellido: this.state.lastname,
+      correo: this.state.email,
+      passwd: this.state.password,
+      telefono: this.state.phone,
+    };
+    axios.post('http://localhost:3300/customer', { user });
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div className='row'>
           <div className='form-control col col-lg-6'>
             <label className=''>Nombre</label>
-            <input type='text' placeholder='María' />
+            <input
+              name='name'
+              onChange={this.handleChange}
+              type='text'
+              placeholder='María'
+            />
           </div>
           <div className='form-control col col-lg-6'>
             <label>Apellido</label>
-            <input type='text' placeholder='López' />
+            <input
+              name='lastname'
+              onChange={this.handleChange}
+              type='text'
+              placeholder='López'
+            />
           </div>
         </div>
         <div className='form-control'>
           <label>Correo electrónico</label>
-          <input type='text' placeholder='ejemplo@dominio.com' />
+          <input
+            name='email'
+            onChange={this.handleChange}
+            type='text'
+            placeholder='ejemplo@dominio.com'
+          />
         </div>
         <div className='form-control'>
           <label>Contraseña</label>
-          <input type='password' placeholder='•••••••••••••' />
+          <input
+            name='password'
+            onChange={this.handleChange}
+            type='password'
+            placeholder='•••••••••••••'
+          />
         </div>
         <div className='form-control'>
           <label>Confirma tu contraseña</label>
@@ -35,6 +84,8 @@ class RegisterForm extends Component {
           <label>Teléfono</label>
           <div className='row'>
             <input
+              name='phone'
+              onChange={this.handleChange}
               type='text'
               value='504'
               className='align-right col   col-lg-2'
